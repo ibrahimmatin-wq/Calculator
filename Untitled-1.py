ad93328336eb63de4f269cdb7c1f0c8413c9a4ef
ibrahimmatin-wq <ibrahimmatinch@gmail.com>
2025-10-05 01:58:56 -0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,25 @@
+print("First project of making a calculator"
+      "using python programming language")
+# Calculator
+a=int(input("Enter first number:")) # 17
+b=int(input("Enter second number:")) # 4            
+print("Enter 1 for addition")
+print("Enter 2 for subtraction")            
+print("Enter 3 for multiplication")
+print("Enter 4 for division")
+print("Enter 5 for modulus")
+operation=int(input("Enter your operation:")) # 3
+if operation==1:
+    print("The addition is:", a+b)      
+elif operation==2:
+    print("The subtraction is:", a-b)
+elif operation==3:
+    print("The multiplication is:", a*b)
+elif operation==4:
+    print("The division is:", a/b)  
+elif operation==5:
+    print("The modulus is:", a%b)
+else:
+    print("Invalid operation")
+
+            
